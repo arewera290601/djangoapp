cf21d1277ee9722707c25759c48ad73cf825489d
arewera290601 <aleksandrarewera@gmail.com>
2019-10-31 14:20:07 +0100

    fs

--- studenci/urls.py
+++ studenci/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('news/', views.news, name='news'),
     path('miasta/', views.miasta, name='miasta'),
     path('uczelnia/', views.uczelnia, name='uczelnia'),
+    path('login/', views.login, name='login'),
+    path('login2/', views.login2, name='login2'),
 
 ]
--- studenci/views.py
+++ studenci/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from studenci.models import Miasto, Uczelnia
 from django.contrib import messages
+from studenci.form import StudentLoginForm
+from studenci.form import UczelniaForm
 
 
 def index(request):
@@ -36,8 +38,12 @@ def uczelnia(request):
 
     if request.method == 'POST':
         nazwa= request.POST.get('nazwa')
-        u = Uczelnia(nazwa=nazwa)
-        u.save()
+        if len(nazwa.strip()):
+            u = Uczelnia(nazwa=nazwa)
+            u.save()
+            messages.error(request, "Dane zapisano!")
+        else:
+            messages.error(request, "Błędne dane deb***!")
 
     uczelnia = Uczelnia.objects.all()
     kontekst = {
@@ -45,3 +51,38 @@ def uczelnia(request):
     }
     return render(request, 'studenci/uczelnia.html', kontekst)
 
+def login(request):
+
+    if request.method == 'POST':
+        nazwa= request.POST.get('nazwa')
+        kod= request.POST.get('kod')
+        if len(nazwa.strip()):
+            m = Miasto(nazwa=nazwa, kod=kod)
+            m.save()
+            messages.success(request, "Dane zapisano!")
+        else:
+            messages.error(request, "Błędne dane!")
+    else:
+        form = StudentLoginForm()
+
+
+    kontekst = { 'form': form }
+    return render(request, 'studenci/login.html', kontekst)
+
+def login2(request):
+
+    if request.method == 'POST':
+        nazwa= request.POST.get('nazwa')
+        kod= request.POST.get('kod')
+        if len(nazwa.strip()):
+            m = Miasto(nazwa=nazwa, kod=kod)
+            m.save()
+            messages.success(request, "Dane zapisano!")
+        else:
+            messages.error(request, "Błędne dane!")
+    else:
+        form = UczelniaForm()
+
+
+    kontekst = { 'form': form }
+    return render(request, 'studenci/login2.html', kontekst)
